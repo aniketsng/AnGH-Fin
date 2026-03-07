3defa05d9d51a92039c6f7c60f70ad9364e2ab3e
ANIKET SINGH <157113268+aniketsng@users.noreply.github.com>
2026-03-07 22:34:36 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = "angh-ledger-v1";
+
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open(cacheName).then(cache => {
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(response => {
+return response || fetch(e.request);
+})
+);
+});
